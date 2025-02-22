da0fc6c0f5169aeb51eb37e0e834f0af4d6a9a6d
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-02-22 23:55:27 +0530

    created Header,HomePage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
 import './App.css'
+import HomePage from './components/HomePage'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      
-    </>
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header/>
+        <HomePage/>
+      </section>
+    </div>
   )
 }
 
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center pb-20'> 
+        <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl justify-center'>
+            Free<span className='text-blue-400 bold'>Scribe</span>
+        </h1>
+
+        <h3 className='font-medium md:text-lg'>Record
+            <span className='text-blue-400'>&rarr;</span>Transcribe
+            <span className='text-blue-400'>&rarr;</span>Translate
+        </h3>
+
+        <button className='flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+            <p>Record</p>
+            <i className='fa-solid fa-microphone'></i>
+        </button>
+    </main>
+  )
+}
